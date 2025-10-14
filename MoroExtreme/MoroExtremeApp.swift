29fcb4cd591e5b769701b241f2c85a41983835a6
Developer <Lepiozdyx@gmail.com>
2025-10-14 09:45:34 -0300

    Initial Commit

---
+++ MoroExtreme/MoroExtremeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoroExtremeApp.swift
+//  MoroExtreme
+//
+//  Created by Alex on 14.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct MoroExtremeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
